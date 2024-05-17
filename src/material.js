3d87d188039fb3035f4616b5230e3dceab3ed372
boytchev <70705048+boytchev@users.noreply.github.com>
2024-05-17 09:02:02 +0300

    Added AO maps

--- src/material.js
+++ src/material.js
@@ -11,103 +11,126 @@ const // general patch for vertex shader
 	VERTEX_FROM = `void main() {`,
 	VERTEX_TO = `
 		varying vec3 vEquiNoise_Position;
-
 		void main() {
-
 			vEquiNoise_Position = vec3(position);
-		`;
+	`;
 		
 const // general patch for fragment shader
 	FRAGMENT_FROM = `uniform vec3 diffuse;`,
 	FRAGMENT_TO = `
 		varying vec3 vEquiNoise_Position;
-
 		uniform vec3 diffuse;
-		`;
+	`;
 		
 const // specific patch for map in fragment shader
 	FRAGMENT_MAP_FROM = `#include <map_fragment>`,
 	FRAGMENT_MAP_TO = `
 		#ifdef USE_MAP
-		
-		  vec2 EquiNoise_uv = equirectUv(normalize(vEquiNoise_Position.xyz));
-
-		  diffuseColor *= texture2D(map,EquiNoise_uv);
+			vec2 EquiNoise_uv = equirectUv(normalize(vEquiNoise_Position.xyz));
+			diffuseColor *= texture2D(map,EquiNoise_uv);
 		#endif
-		`;
+	`;
 		
 const // specific patch for bumpMap in fragment shader
-	FRAGMENT_BUMPMAP_FROM = `#include <bumpmap_pars_fragment>`,
+	FRAGMENT_BUMPMAP_FROM = `#include <uv_pars_fragment>`,
 	FRAGMENT_BUMPMAP_TO = `
 		#ifdef USE_BUMPMAP
-	uniform sampler2D bumpMap;
-	uniform float bumpScale;
-
-	// Bump Mapping Unparametrized Surfaces on the GPU by Morten S. Mikkelsen
-	// https://mmikk.github.io/papers3d/mm_sfgrad_bump.pdf
-
-	// Evaluate the derivative of the height w.r.t. screen-space using forward differencing (listing 2)
-
-	vec2 dHdxy_fwd() {
-		vec2 EquiNoise_BumpMapUv = equirectUv(normalize(vEquiNoise_Position.xyz));
-
-		vec2 dSTdx = dFdx( EquiNoise_BumpMapUv );
-		vec2 dSTdy = dFdy( EquiNoise_BumpMapUv );
-
-		float Hll = bumpScale * texture2D( bumpMap, EquiNoise_BumpMapUv ).x;
-		float dBx = bumpScale * texture2D( bumpMap, EquiNoise_BumpMapUv + dSTdx ).x - Hll;
-		float dBy = bumpScale * texture2D( bumpMap, EquiNoise_BumpMapUv + dSTdy ).x - Hll;
-
-		return vec2( dBx, dBy );
-
-	}
-
-	vec3 perturbNormalArb( vec3 surf_pos, vec3 surf_norm, vec2 dHdxy, float faceDirection ) {
-
-		// normalize is done to ensure that the bump map looks the same regardless of the texture's scale
-		vec3 vSigmaX = normalize( dFdx( surf_pos.xyz ) );
-		vec3 vSigmaY = normalize( dFdy( surf_pos.xyz ) );
-		vec3 vN = surf_norm; // normalized
-
-		vec3 R1 = cross( vSigmaY, vN );
-		vec3 R2 = cross( vN, vSigmaX );
-
-		float fDet = dot( vSigmaX, R1 ) * faceDirection;
-
-		vec3 vGrad = sign( fDet ) * ( dHdxy.x * R1 + dHdxy.y * R2 );
-		return normalize( abs( fDet ) * surf_norm - vGrad );
-
-	}
-
+			#define EQUINOISE_USE_BUMPMAP
+			#undef USE_BUMPMAP
+			vec2 vBumpMapUv;
 		#endif
-		`;
+		#include <uv_pars_fragment>
+		#ifdef EQUINOISE_USE_BUMPMAP
+			#define USE_BUMPMAP
+			#undef EQUINOISE_USE_BUMPMAP
+		#endif
+	`;
+
+const // another specific patch for bumpMap in fragment shader
+	FRAGMENT_BUMPMAP_FROM2 = `void main() {`,
+	FRAGMENT_BUMPMAP_TO2 = `void main() {
+		#ifdef USE_BUMPMAP
+			vBumpMapUv = equirectUv(normalize(vEquiNoise_Position.xyz));
+		#endif
+	`;
+		
+const // specific patch for aoMap in fragment shader
+	FRAGMENT_AOMAP_FROM = `#include <uv_pars_fragment>`,
+	FRAGMENT_AOMAP_TO = `
+		#ifdef USE_AOMAP
+			#define EQUINOISE_USE_AOMAP
+			#undef USE_AOMAP
+			vec2 vAoMapUv;
+		#endif
+		#include <uv_pars_fragment>
+		#ifdef EQUINOISE_USE_AOMAP
+			#define USE_AOMAP
+			#undef EQUINOISE_USE_AOMAP
+		#endif
+	`;
+
+const // another specific patch for аоMap in fragment shader
+	FRAGMENT_AOMAP_FROM2 = `void main() {`,
+	FRAGMENT_AOMAP_TO2 = `void main() {
+		#ifdef USE_AOMAP
+			vAoMapUv = equirectUv(normalize(vEquiNoise_Position.xyz));
+		#endif
+	`;
 
 	
+// replace a group of strings as a transaction, i.e. either all replacements are done, or none is done
+function batchReplace( string, from, to, name )
+{
+	// check all patterns exist
+	for( var i in from )
+	{
+		if( string.indexOf(from[i]) < 0 )
+		{
+			console.warn( `Patch №${i} of ${name} cannot be applied, so all patches of ${name} will be ignored.` );
+			return string;
+		}
+	} // for i
+	
+	// replace patterns
+	for( var i in from )
+	{
+		string = string.replace( from[i], to[i] );
+	}
+	
+	return string;
+}
+
+
 	
 // patches material's shaders
 function equimaterial( material )
 {
 	var map = material.map?.mapping == EquirectangularReflectionMapping;
 	var bumpMap = material.bumpMap?.mapping == EquirectangularReflectionMapping;
+	var aoMap = material.aoMap?.mapping == EquirectangularReflectionMapping;
 	
-	if( map || bumpMap )
+	if( map || bumpMap || aoMap )
 	{
 		material.onBeforeCompile = function( shader )
 		{
 			// general patches for any kind of map -- add a varying variable
 			// containing position used for equrectangular textures
-			shader.vertexShader = shader.vertexShader.replace( VERTEX_FROM, VERTEX_TO );
-			shader.fragmentShader = shader.fragmentShader.replace( FRAGMENT_FROM, FRAGMENT_TO );
+			shader.vertexShader = batchReplace( shader.vertexShader, [VERTEX_FROM], [VERTEX_TO], 'vertex' );
+			shader.fragmentShader = batchReplace( shader.fragmentShader, [FRAGMENT_FROM], [FRAGMENT_TO], 'fragment' );
 			
 			// patch various maps in fragment shader
 			
 			if( map )
 			{
-				shader.fragmentShader = shader.fragmentShader.replace( FRAGMENT_MAP_FROM, FRAGMENT_MAP_TO );
+				shader.fragmentShader = batchReplace( shader.fragmentShader, [FRAGMENT_MAP_FROM], [FRAGMENT_MAP_TO], 'map' );
 			} else
 			if( bumpMap )
 			{
-				shader.fragmentShader = shader.fragmentShader.replace( FRAGMENT_BUMPMAP_FROM, FRAGMENT_BUMPMAP_TO );
+				shader.fragmentShader = batchReplace( shader.fragmentShader, [FRAGMENT_BUMPMAP_FROM, FRAGMENT_BUMPMAP_FROM2], [FRAGMENT_BUMPMAP_TO, FRAGMENT_BUMPMAP_TO2], 'bumpMap' );
+			} else
+			if( aoMap )
+			{
+				shader.fragmentShader = batchReplace( shader.fragmentShader, [FRAGMENT_AOMAP_FROM, FRAGMENT_AOMAP_FROM2], [FRAGMENT_AOMAP_TO, FRAGMENT_AOMAP_TO2], 'aoMap' );
 			}
 		} // onBeforeCompile
 	} // if any map	
