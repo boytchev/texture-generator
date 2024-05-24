a6a04e84896979a82c2911c8ccd755cc7a54f39f
boytchev <70705048+boytchev@users.noreply.github.com>
2024-05-24 10:58:00 +0300

    Refactored "Zebra lines"

--- docs/patterns.md
+++ docs/patterns.md
@@ -6,6 +6,7 @@
 * [Polka dots](#polka-dots) &ndash; fashion design with dots
 * [Simplex noise](#simplex-noise) &ndash; gradient noise pattern
 * [Stars](#stars) &ndash; night sky stars
+* [Zebra lines](#zebra-lines) &ndash; alternating white and black stripes
 
 
 Each predefined pattern is implemented as JS module file, exporting the same set of functions:
@@ -204,6 +205,31 @@ options = {
 	variation: 0,
 }
 ```
+
+
+
+
+
+
+# Zebra lines
+<img src="images/zebra-lines.jpg">
+
+[<img src="images/zebra-lines-1.png">](https://boytchev.github.io/texture-generator/online/zebra-lines?a=0&r=9&s=80) [<img src="images/zebra-lines-2.png">](https://boytchev.github.io/texture-generator/online/zebra-lines?a=45&r=9&s=30)  [<img src="images/zebra-lines-3.png">](https://boytchev.github.io/texture-generator/online/zebra-lines?a=-45&r=9&s=94) 
+
+#### Options:
+
+* `resolution` (`r`) &ndash; integer; the resolution of the texture is 2<sup>r</sup>&times;2<sup>r-1</sup>
+* `size` (`s`) &ndash; number; the visual size of the pattern &#x2208; [0,100]
+* `angle` (`a`) &ndash; number; the angle of pattern tilt &#x2208; [-180&deg;,180&deg;]
+
+#### Defaults:
+
+```js
+options = {
+	resolution: 9, // 512x256
+	size: 50,
+	angle: 0,
+}
 
 
 
--- src/patterns/zebra-lines.js
+++ src/patterns/zebra-lines.js
@@ -1,26 +1,20 @@
 ﻿
+//	Procedural Equirectangular Textures
+//	Zebra lines Pattern
+//
+//	pattern( ... )	- implements Zebra lines pattern
+//	options( opt )	- converts options into internal format
+//	share( opt )	- converts options into URL
+//	info			- general info for the generator
 
-import { Vector3, Color, MathUtils } from "three";
+
+
+import { Vector3, MathUtils } from "three";
 import { noise } from "../noise.js";
 
 
 
-function options( opt )
-{
-	var options = { };
-		
-	var angle = (opt.angle??0)*Math.PI/180;
-	
-	options.size = 100 - (opt.size??50);
-	options.up = new Vector3( 0, 1e-6*Math.round(1e6*Math.cos( angle )), 1e-6*Math.round(1e6*Math.sin( angle )) );
-
-	return options;
-}
-	
-
-
 var vec = new Vector3( );
-var limit = 0.01;
 
 function pattern( x, y, z, color, options, /*u, v, px, py, width, height*/ )
 {
@@ -37,23 +31,41 @@ function pattern( x, y, z, color, options, /*u, v, px, py, width, height*/ )
 
 
 
-function share( options )
+function options( opt )
+{
+	var options = { };
+		
+	var angle = (opt.angle??0)*Math.PI/180;
+	
+	options.size = MathUtils.mapLinear( opt.size, 0, 100, 151, 2 );
+	
+	options.up = new Vector3( 0, 1e-6*Math.round(1e6*Math.cos( angle )), 1e-6*Math.round(1e6*Math.sin( angle )) );
+
+	return options;
+}
+	
+
+
+function share( opt )
 {
 	var params = [];
 	
-	params.push( `a=${options.angle}` );
-	params.push( `r=${options.resolution}` );
-	params.push( `s=${options.size}` );
+	params.push( `a=${opt.angle}` );
+	params.push( `r=${opt.resolution}` );
+	params.push( `s=${opt.size}` );
 
 	params = params.join( '&' );
 	return window.location.href.split('?')[0].split('#')[0] + '?' + params;
 }
 
 
+
 var info = {
 		name: 'Zebra lines',
+		info: 'Designed for .map properties',
 		lightIntensity: 5,
 	};
+
 
 
 export { pattern, options, share, info };
