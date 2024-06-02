a15d9a369c5f795f7bb1ac3eb723115ce3bfdc81
boytchev <70705048+boytchev@users.noreply.github.com>
2024-06-02 13:29:26 +0300

    Added "Clouds"

---
+++ docs/clouds.md
@@ -0,0 +1,79 @@
+<img class="logo" src="../assets/logo/logo.png">
+
+
+# Procedural Equirectangular Textures
+
+
+## Clouds
+<img src="images/clouds.jpg">
+
+This texture renders clouds. The texture has alpha channel
+to support transparency and opacity. Click on a snapshot to
+open it online.
+
+<p class="gallery">
+
+	<a class="style-block nocaption" href="../online/clouds.html?width=512&height=256&scale=43&density=54&opacity=79&color=16777215&subcolor=10526896">
+		<img src="images/clouds-1.png">
+	</a>
+
+	<a class="style-block nocaption" href="../online/clouds.html?width=512&height=256&scale=11&density=63&opacity=71&color=16062464&subcolor=16771584">
+		<img src="images/clouds-2.png">
+	</a>
+
+	<a class="style-block nocaption" href="../online/clouds.html?width=512&height=256&scale=47&density=81&opacity=55&color=16777215&subcolor=1966335">
+		<img src="images/clouds-3.png">
+	</a>
+
+</p>
+
+
+### Code example
+
+Code template of parameters with their default values.
+
+```js
+import * as PET from "pet/patterns/clouds.js";
+:
+model.material.map = PET.texture( );
+PET.material( model.material );
+```
+
+
+### Parameters
+
+The parameters of the texture generator are:
+
+* `width` &ndash; texture width in pixels, default 512
+* `height` &ndash; texture height in pixels, default 256
+* `scale` &ndash; relative could size [0,100], default 50
+* `density` &ndash; density of clouds [0,100], default 50
+* `opacity` &ndash; opacity of clouds [0,100], default 80
+* `color` &ndash; color of dots, default 0xFFFFFF (white)
+* `subcolor` &ndash; secondary color, default 0xA0A0B0 (pale blue)
+
+
+### API
+
+All texture modules share the same API.
+
+* `pattern( x, y, z, color, options )` &ndash; pattern implementation
+* `texture( {params} )` &ndash; generator for a texture with given parameters
+* `defaults` &ndash; object with default parameters
+* `material( ... )` &ndash; material shader patcher
+
+
+### Online generator
+
+[online/clouds.html](../online/clouds.html)
+
+
+### Source
+
+[src/patterns/clouds.js](https://github.com/boytchev/texture-generator/blob/main/src/patterns/clouds.js)
+
+
+		
+<div class="footnote">
+	<a href="#" onclick="window.history.back(); return false;">Back</a>
+</div>
---
+++ src/patterns/clouds.js
@@ -0,0 +1,93 @@
+﻿
+//	Procedural Equirectangular Textures
+//	Clouds Pattern
+//
+//	defaults = {...}	- default parameters
+//	pattern( ... )		- calculate color of pixel
+//	texture( params )	- generate a texture
+//	material( ... )		- material shader fix
+
+
+
+import { Color } from "three";
+import { noise, texture as coreTexture } from "pet/texture-generator.js";
+
+
+
+var defaults = {
+		$name: 'Simplex noise',
+		
+		width: 512,
+		height: 256,
+		
+		scale: 50,
+		density: 50,
+		opacity: 80,
+		
+		color: 0xFFFFFF,
+		subcolor: 0xA0A0B0,
+	};
+	
+	
+	
+function pattern( x, y, z, color, options, /*u, v, px, py*/ )
+{
+	var k = -0.7 + options.density
+		+ 0.4*noise( options.scale*x, options.scale*y, options.scale*z )
+		+ 0.2*noise( 2*options.scale*x, 2*options.scale*y, 2*options.scale*z )*options.opacity
+		+ 0.05*noise( 6*options.scale*x, 6*options.scale*y, -6*options.scale*z )*options.opacity*options.opacity
+		+ 0.03*noise( 8*options.scale*x, -8*options.scale*y, 8*options.scale*z )*options.opacity*options.opacity;
+	
+	k = 0.5*k + 0.3*k**2 + 0.4*k**3;
+	if( k<0.3 ) k=(k/0.3)**4*0.3;
+	
+	var n = 0.5 - 0.5*noise( options.scale*(x+0.1), options.scale*y, options.scale*z )
+	var m = n**0.4;
+	
+	color.lerpColors( options.subcolor, options.color, m );
+	color.a = Math.min(k*n,1)**0.75*options.opacity;
+}
+
+
+
+function options( params )
+{
+	var options = { };
+	
+	
+	options.scale = 2**( (params.scale??defaults.scale)/100 * Math.log2(0.5/6) + Math.log2(6) );
+
+	options.density = -((params.density??defaults.density)-50)/50;
+	options.opacity = (params.opacity??defaults.opacity)/100;
+
+	options.color = new Color( params.color ?? defaults.color );
+	options.subcolor = new Color( params.subcolor ?? defaults.subcolor );
+	
+	options.width = params.width ?? defaults.width;
+	options.height = params.height ?? defaults.height;
+	
+	return options;
+}
+	
+
+
+function texture( ...opt )
+{
+	if( opt.length==0 ) opt = [defaults];
+	
+	// if there is {...}, assume it is user options, compile them
+	var params = opt.map( (e) => (e!=-null) && (typeof e =='object') && !(e instanceof HTMLCanvasElement) ? options(e) : e );
+
+	// if pattern is missing, add pattern
+	if( params.findIndex((e)=>e instanceof Function) == -1 )
+	{
+		params.push( pattern );
+	}
+		
+	return coreTexture( ... params );
+}
+
+
+
+export { pattern, defaults, texture };
+export { material } from "pet/texture-generator.js";
